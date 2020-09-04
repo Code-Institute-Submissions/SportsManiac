778cba2a43dc416fdfcb7c6c8645be4dbdfbed9c
Cameron Duncan <cameronduncan678@gmail.com>
2020-09-04 08:53:41 +0100

    gulp watch setup and init styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglifycss = require('gulp-uglifycss');
+const sourcemaps = require('gulp-sourcemaps');
 
+gulp.task('test', (done) => {
+  console.log('Gulp is Running');
+  done();
+});
+
+gulp.task('sass', (done) => {
+  return gulp
+    .src('./assets/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(concat('main.css'))
+    .pipe(
+      uglifycss({
+        maxLineLen: 80,
+        uglyComments: true,
+      })
+    )
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./assets/css/compiled'));
+  done();
+});
+
+gulp.task('watch', () => {
+  gulp.watch(['./assets/scss/**/*.scss', '**/*.html'], gulp.series(['sass']));
+});
--- initDev/axiosTests/axios.js
+++
@@ -1,6 +0,0 @@
-axios
-  .get('https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=Boston')
-  .then((response) => {
-    console.log(response.data.teams[3]);
-    $('#data').html(response.data.teams[3].strDescriptionEN);
-  });
