f2cdd77b138fe1adc4df03ad263828e858794ed0
Cameron Duncan <cameronduncan678@gmail.com>
2020-09-24 21:02:59 +0100

    filter sport function added

--- js/app.js
+++ js/app.js
@@ -3,6 +3,22 @@ function searchForTeam(input) {
   getTeams(input, buildAccordions);
 }
 
+function filterSport(input, sport) {
+  if (sport == 'Baseball') {
+    getTeams(input, baseballAccordion);
+  } else if (sport == 'Soccer') {
+    getTeams(input, soccerAccordion);
+  } else if (sport == 'American Football') {
+    getTeams(input, footballAccordion);
+  } else if (sport == 'Basketball') {
+    getTeams(input, basketBallAccordion);
+  } else if (sport == 'Ice Hockey') {
+    getTeams(input, hockeyAccordion);
+  } else if (sport == 'Rugby') {
+    getTeams(input, rugbyAccordion);
+  }
+}
+
 function buildAccordions(dataArr) {
   accordionArray = '';
   if (dataArr) {
@@ -18,12 +34,150 @@ function buildAccordions(dataArr) {
   jQuery('#accordions').html(accordionArray);
 }
 
+function baseballAccordion(dataArr) {
+  accordionArray = '';
+  if (dataArr) {
+    dataArr.forEach((obj) => {
+      if (obj.strSport == 'Baseball')
+        accordionArray += addAccordion(
+          obj.strTeamLogo,
+          obj.strTeam,
+          obj.strTeamBadge,
+          obj.strDescriptionEN
+        );
+    });
+  }
+  jQuery('#accordions').html(accordionArray);
+}
+
+function soccerAccordion(dataArr) {
+  accordionArray = '';
+  if (dataArr) {
+    dataArr.forEach((obj) => {
+      if (obj.strSport == 'Soccer')
+        accordionArray += addAccordion(
+          obj.strTeamLogo,
+          obj.strTeam,
+          obj.strTeamBadge,
+          obj.strDescriptionEN
+        );
+    });
+  }
+  jQuery('#accordions').html(accordionArray);
+}
+
+function footballAccordion(dataArr) {
+  accordionArray = '';
+  if (dataArr) {
+    dataArr.forEach((obj) => {
+      if (obj.strSport == 'American Football')
+        accordionArray += addAccordion(
+          obj.strTeamLogo,
+          obj.strTeam,
+          obj.strTeamBadge,
+          obj.strDescriptionEN
+        );
+    });
+  }
+  jQuery('#accordions').html(accordionArray);
+}
+
+function basketBallAccordion(dataArr) {
+  accordionArray = '';
+  if (dataArr) {
+    dataArr.forEach((obj) => {
+      if (obj.strSport == 'Basketball')
+        accordionArray += addAccordion(
+          obj.strTeamLogo,
+          obj.strTeam,
+          obj.strTeamBadge,
+          obj.strDescriptionEN
+        );
+    });
+  }
+  jQuery('#accordions').html(accordionArray);
+}
+
+function hockeyAccordion(dataArr) {
+  accordionArray = '';
+  if (dataArr) {
+    dataArr.forEach((obj) => {
+      if (obj.strSport == 'Ice Hockey')
+        accordionArray += addAccordion(
+          obj.strTeamLogo,
+          obj.strTeam,
+          obj.strTeamBadge,
+          obj.strDescriptionEN
+        );
+    });
+  }
+  jQuery('#accordions').html(accordionArray);
+}
+
+function rugbyAccordion(dataArr) {
+  accordionArray = '';
+  if (dataArr) {
+    dataArr.forEach((obj) => {
+      if (obj.strSport == 'Rugby')
+        accordionArray += addAccordion(
+          obj.strTeamLogo,
+          obj.strTeam,
+          obj.strTeamBadge,
+          obj.strDescriptionEN
+        );
+    });
+  }
+  jQuery('#accordions').html(accordionArray);
+}
+
 //Document app functions
 $(document).ready(() => {
   $('#search-btn').click(() => {
     var input = $('#search-input').val();
     if (input) {
       searchForTeam(input);
+    }
+  });
+
+  $('#baseball').click(() => {
+    var input = $('#search-input').val();
+    if (input) {
+      filterSport(input, 'Baseball');
+    }
+  });
+
+  $('#soccer').click(() => {
+    var input = $('#search-input').val();
+    if (input) {
+      filterSport(input, 'Soccer');
+    }
+  });
+
+  $('#football').click(() => {
+    var input = $('#search-input').val();
+    if (input) {
+      filterSport(input, 'American Football');
+    }
+  });
+
+  $('#basketball').click(() => {
+    var input = $('#search-input').val();
+    if (input) {
+      filterSport(input, 'Basketball');
+    }
+  });
+
+  $('#hockey').click(() => {
+    var input = $('#search-input').val();
+    if (input) {
+      filterSport(input, 'Ice Hockey');
+    }
+  });
+
+  $('#rugby').click(() => {
+    var input = $('#search-input').val();
+    if (input) {
+      filterSport(input, 'Rugby');
     }
   });
 });
